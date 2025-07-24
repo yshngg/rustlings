56431d64e1dd5621c7d595d2548cdc017cda7d9a
yshngg <yshngg@outlook.com>
2025-07-24 16:04:13 +0800

    backup

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -1,5 +1,5 @@
 // Calls of this function should be replaced with calls of `string_slice` or `string`.
-fn placeholder() {}
+fn placeholder(s: Display) {}
 
 fn string_slice(arg: &str) {
     println!("{arg}");
